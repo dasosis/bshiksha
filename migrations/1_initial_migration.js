4f7d05637a0f2342058c34659b5c4a108172ef4f
dasosis <soumyasnigdhadas@gmail.com>
2024-04-20 22:26:15 +0530

    Co-authored-by: SANKET SAHA <Sanket-Saha-768@users.noreply.github.com>

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -2,6 +2,4 @@ const BShiksha = artifacts.require("BShiksha");
 
 module.exports = async function(deployer) {
     await deployer.deploy(BShiksha);
-    const deployedInstance = await BShiksha.deployed();
-    console.log("Contract deployed at address:", deployedInstance.address);
 };
