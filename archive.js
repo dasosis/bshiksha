c109f53be9f8c0b75d697d2720b4c38fda3f206d
dasosis <soumyasnigdhadas@gmail.com>
2024-04-21 17:11:59 +0530

    updated archive

--- archive.js
+++ archive.js
@@ -1,86 +1,90 @@
-import express from 'express';
-import path from 'path';
-import pkg from 'body-parser';
-import multer from 'multer';
-import { create } from 'kubo-rpc-client';
-import Web3 from 'web3';
-import artifact from '../../build/contracts/BShiksha.json' assert { type: 'json' };
-import detectProvider from '@metamask/detect-provider';
+if (typeof window.ethereum !== 'undefined') {
+    const web3 = new Web3(window.ethereum);
+    connectAccount().then(currentAccount => {
+        getContract(web3).then(({ contractInstance, address, networkId }) => {
+            uploadPostToBlock(web3, contractInstance, currentAccount, address, networkId);
+        }).catch(error => {
+            console.error('Error getting contract:', error);
+        });
+    }).catch(error => {
+        console.error('Error connecting to MetaMask:', error);
+    });
+} else {
+    console.log('MetaMask is not installed');
+}
 
-const { abi } = artifact;
-const contractAddress = '0x942F0f759B31D4928B0A04684000868ab64d6BcB'; // Replace with your contract address
-const { urlencoded } = pkg;
-const app = express();
-const port = 3000;
-const upload = multer();
-const contract = new web3.eth.Contract(abi, contractAddress);
-let web3;
-app.use(urlencoded({ extended: false }));
-
-(async () => {
+async function connectAccount() {
     try {
-        const provider = await detectProvider();
-        if (provider) {
-            web3 = new Web3(provider);
-        } else {
-            console.error('MetaMask not detected');
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const currentAccount = accounts[0];
+        console.log('Connected wallet address:', currentAccount);
+        return currentAccount;
+    } catch (error) {
+        console.error('Error connecting to MetaMask:', error);
+        throw error;
+    }
+}
+
+async function getContract(web3) {
+    try {
+        const response = await fetch('http://localhost:3000/BShiksha.json');
+        if (!response.ok) {
+            throw new Error('Failed to fetch contract artifact');
         }
+        const json = await response.text();
+        const contractArtifact = JSON.parse(json);
+        const abi = contractArtifact.abi;
+        const deployment = Object.keys(contractArtifact.networks);
+        const address = contractArtifact.networks[deployment[deployment.length - 1]];
+        console.log(address.address);
+        const contractInstance = new web3.eth.Contract(abi, address.address);
+        const networkId = await web3.eth.net.getId();
+        return { contractInstance, address: address.address, networkId };
     } catch (error) {
-        console.error('Error detecting MetaMask provider:', error);
-    }
-})();
-
-app.get('/', (req, res) => {
-    const directoryPath = process.cwd();
-    res.sendFile(path.join(directoryPath, 'src', 'index.html'));
-});
-
-app.post('/submit', upload.single('file'), async (req, res) => {
-    try {
-        const { title, description } = req.body;
-        const fileData = req.file.buffer;
-        const { cid } = await uploadFileToIPFS(fileData);
-        console.log(`Title: ${title}`);
-        console.log(`Description: ${description}`);
-        console.log(`File uploaded successfully! CID: ${cid}`);
-        res.send('Form submitted successfully.');
-        await uploadPostToBlock('${cid}', description);
-        // console.log('Post uploaded:', uploadPostTx);
-        // res.send('Post uploaded successfully!');
-    } catch (err) {
-        console.log(err);
-    }
-});
-
-async function uploadFileToIPFS(fileData) {
-    try {
-        const client = create();
-        // console.log(`File content: ${fileData.toString()}`)
-        // const fileData = await fs.(readFileSyncfilePath)
-        const { cid } = await client.add(fileData);
-        return { cid };
-    } catch (error) {
-        console.error('Error uploading file:', error);
+        console.error('Error fetching contract artifact:', error);
+        throw error;
     }
 }
+/*
+async function uploadPostToBlock(web3, contractInstance, currentAccount, address, networkId) {
+    try {
+        console.log('hello block');
+        const transaction = contractInstance.methods.uploadPost(
+            '1',
+            '1'
+        );
+        const gas = await transaction.estimateGas({ from: currentAccount });
+        const gasPrice = await web3.eth.getGasPrice();
+        const data = transaction.encodeABI();
+        const nonce = await web3.eth.getTransactionCount(address, 'pending') ;
+        const privateKey = 'd66c5bd20fcd0a36f215ef901ed9ff1707f6799e1539ada8a84fb98e0c16f5a7';
+        const signedTx = await web3.eth.accounts.signTransaction({
+            to: contractInstance.options.address,
+            data,
+            gas,
+            gasPrice,
+            nonce,
+            chainId: 1337
+        }, privateKey
+        );
+        
+        console.log(gas, gasPrice, data, nonce, address, networkId ,signedTx);
+        const reciept = await web3.eth.sendTransaction(signedTx.rawTransaction);
+        console.log(reciept);
+    } catch (error) {
+        console.error('Error uploading post:', error);
+        throw error;
+    }
+}*/
 
-async function uploadPostToBlock(description, cid) {
-    // try {
-    //     const accounts = await eb3.eth.getAccounts();
-    //     console.log('Current account:', accounts[0]);
-    //     const options = {
-    //         from: accounts[0],
-    //         gas: 8000000
-    //     };
-    //     await contract.methods.uploadPost(cid, description).send(options);
-    //     console.log('Post uploaded');      
-    // }
-    // catch (error) {
-    //     console.log(error);
-    // }
+async function uploadPostToBlock(web3, contractInstance, currentAccount, address, networkId) {
+    const privateKey = 'd66c5bd20fcd0a36f215ef901ed9ff1707f6799e1539ada8a84fb98e0c16f5a7';
+    const address = address;
+    const provider = new Web3.providers.HttpProvider('http://localhost:9545');
+    // const web3 = new Web3(provider);
+    console.log(await contractInstance.methods.data().call());
+    console.log(`Old data value: ${await contractInstance.methods.data().call()}`);
+    const receipt = await myContract.methods.setData(3).send({ from: address });
+    console.log(`Transaction hash: ${receipt.transactionHash}`);
+    console.log(`New data value: ${await contractInstance.methods.data().call()}`);
 }
-
-
-app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
-});
