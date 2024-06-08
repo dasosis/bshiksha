dc679a6aa68d3d7131806a3ac5ebf4e2a38da007
Souradeep Bhattacharya <souradeep102b@gmail.com>
2024-06-08 23:54:00 +0530

    made comments working

--- client/src/components/CommentView/Comment/Comment.jsx
+++ client/src/components/CommentView/Comment/Comment.jsx
@@ -1,15 +1,22 @@
+import { getUserName } from '../../../scripts/block';
+import { useStore } from '../../../dataStore';
 import './Comment.scss';
+import { useEffect } from 'react';
 
-const Comment = () => {
-  const author = 'John Doe';
-  const comment =
-    'lorem ipsum dolor sit amet consectetur. Erat elit urna viverra tincidunt consectetur volutpat nibh quis.  Lectus quis commodo vulputate nulla est purus ac donec. Gravida cursus porttitor posuere sed. Phasellus erat tincidunt proin leo tellus nisi cum magnis ornare.';
-  const time = '2 hours ago';
+const Comment = (comment) => {
+  const { selectedPost } = useStore();
+  // const author = 'John Doe';
+  // const comment =
+  //   'lorem ipsum dolor sit amet consectetur. Erat elit urna viverra tincidunt consectetur volutpat nibh quis.  Lectus quis commodo vulputate nulla est purus ac donec. Gravida cursus porttitor posuere sed. Phasellus erat tincidunt proin leo tellus nisi cum magnis ornare.';
+  // const time = '2 hours ago';
+  const commenter = comment.comment.commenter;
+  const decodedComment = comment.comment.decodedComment;
+  // console.log('Comment:', comment);
   return (
     <div className='Comment'>
-      <div className='comment-author'>{author}</div>
-      <div className='comment-text'>{comment}</div>
-      <div className='comment-time'>{time}</div>
+      <div className='comment-author'>{commenter}</div>
+      <div className='comment-text'>{decodedComment}</div>
+      {/* <div className='comment-time'>{time}</div> */}
     </div>
   );
 };
--- client/src/components/CommentView/CommentView.jsx
+++ client/src/components/CommentView/CommentView.jsx
@@ -1,14 +1,75 @@
 import Comment from './Comment/Comment';
+import { fetchCommentEventsFromBlock, addCommentToBlock } from '../../scripts/block';
+import { useEffect } from 'react';
+import { useStore } from '../../dataStore';
 
 import './CommentView.scss';
 
 const CommentView = () => {
-  const comment = 5;
+  const { selectedPost } = useStore();
+  const { currentAccount } = useStore();
+  const { userComments, setUserComments } = useStore();
+  const fetchComments = async () => {
+    let commentsArray = [];
+    let commenterArray = [];
+    const events = await fetchCommentEventsFromBlock(selectedPost.id);
+    console.log('Logging Events from post.js', events, selectedPost.id);
+    const commentCidArray = [];
+    for (const event of events) {
+      const { commentCid, commenter } = event.returnValues;
+      console.log('post.js print comment before server', { commentCid, commenter });
+      commentCidArray.push(commentCid);
+      commenterArray.push(commenter);
+    }
+    const commentResponse = await fetch('http://localhost:3000/commentDecode', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json', // Set content type to JSON
+      },
+      body: JSON.stringify({ commentCids: commentCidArray }),
+    });
+    const commentData = await commentResponse.json();
+    const commentDataWithCommenter = commentData.map((comment, index) => ({
+      ...comment,
+      commenter: commenterArray[index],
+    }));
+
+    commentsArray = commentsArray.concat(commentDataWithCommenter);
+
+    setUserComments(commentsArray);
+  };
+
+  useEffect(() => {
+    fetchComments();
+  }, [selectedPost]);
+
+  const handleComment = async () => {
+    const comment = document.querySelector('.commentInput input').value.trim();
+    if (!comment) return;
+    const response = await fetch('http://localhost:3000/comment', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json', // Set content type to JSON
+      },
+      body: JSON.stringify({ comment }),
+    });
+    const commentCid = await response.json();
+    console.log(commentCid);
+    await addCommentToBlock(selectedPost.id, commentCid, currentAccount);
+    fetchComments();
+    document.querySelector('.commentInput input').value = '';
+    // setUserComments([...userComments, commentCid]);
+  };
+
   return (
     <div className='CommentView'>
-      {[...Array(comment)].map((value, index) => (
-        <Comment key={index} />
+      {userComments.map((value) => (
+        <Comment comment={value} />
       ))}
+      <div className='commentInput'>
+        <input type='text' placeholder='Add a comment...' />
+        <button onClick={handleComment}>Submit</button>
+      </div>
     </div>
   );
 };
